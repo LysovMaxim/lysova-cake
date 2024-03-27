b9c47092d8af48edf7204e6d32f90aded9472ed8
LysovMaxim <max_maran@icloud.com>
2024-03-28 01:32:24 +0200

    created the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Header } from './Header';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+    </>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,28 @@
+import { ReactComponent as Logo } from '../../images/logo.svg';
+import { ReactComponent as Basket } from '../../images/basket.svg';
+import { FaPhone } from 'react-icons/fa6';
+import css from './Header.module.css';
+
 export const Header = () => {
   return (
-    <div>
-      <p>привет</p>
-    </div>
+    <header>
+      <div className={css.container}>
+        <Logo className={css.logo} />
+        <ul className={css.headerList}>
+          <li className={css.headerListElement}>Про мене</li>
+          <li className={css.headerListElement}>Торт за ескізом</li>
+          <li className={css.headerListElement}>Каталог</li>
+          <li className={css.headerListElement}>Ціни</li>
+          <li className={css.headerListElement}>Контакти</li>
+          
+        </ul>
+        <Basket className={css.headerBasket} />
+        <a href="tel:+380730368141" className={css.headerTel}>
+          <FaPhone size={18} />
+          073-036-81-41
+        </a>
+      </div>
+    </header>
   );
 };
 
