0249349dec8242cd70c75949580aa9394872d5b2
LysovMaxim <max_maran@icloud.com>
2024-03-28 18:51:41 +0200

    add section Hero and add photo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,19 @@
 import css from './Hero.module.css';
+import bubblesTitle from "../../images/png-bubbles-title.png"
+import photoTitle from "../../images/photo-title.jpg"
 
 export const Hero = () => {
-  return <section className={css.hero}></section>;
+  return (
+    <section className={css.hero}>
+      <div className={css.container}>
+              <h1 className={css.heroTitle}>Авторські торти та десерти для вашого свята</h1>
+              <p className={css.heroInfo}>Виконаю замовлення будь-якої складності та дизайну. Знаходжусь с.Нові Петрівці</p>
+              <button className={css.heroBtn}><div className={css.heroBtnDecor}>Замовити торт</div></button>
+              <img className={css.heroBubblesTitle} src={bubblesTitle} alt="bubbles" />
+              <img className={css.heroPhotoTitle} src={photoTitle} alt="photoCake" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
